e387c2054008e710799571dedcbdf5a9803d0187
SeokMin <lsm@unison.co.kr>
2021-03-19 16:39:57 +0900

    4673 11596

---
+++ Func/BaekJoon_15596.py
@@ -0,0 +1,10 @@
+# 15596번
+def solve(a):
+    sum = 0
+    for i in a:
+        sum += i
+    return sum
+
+# 다른 사람 코드
+def solve(a):
+    return sum(a)
---
+++ Func/BaekJoon_4673_Fail.py
@@ -0,0 +1,25 @@
+# 4673번
+
+def selfNumber(n):
+    arr = list()
+    res = 0
+    for i in range(1, 10001):
+        if i < 10:
+            res = i + 1
+        elif i < 100:
+            res = i + (i // 10) + (i % 10)
+            arr.append(res)
+        elif i < 1000:
+            res = i + (i // 100) + ((i % 100) // 10) + ((i % 100) % 10)
+            arr.append(res)
+        elif i < 10000:
+            res = i + (i // 1000) + ((i % 1000) // 100) + (((i % 1000) % 100) // 10) + (((i % 1000) % 100) % 10)
+            if res <= 10000:
+                arr.append(res)
+        arr.sort()
+        arr1 = [i for i in range(1, 10001)]
+        notSelf = [item for item in arr1 if item not in arr]
+        for each in notSelf:
+            print(each)
+
+selfNumber()
